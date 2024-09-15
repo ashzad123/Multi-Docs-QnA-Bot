358e2100b2f6823a14bafa78917ac34978001cf3
ashzad123 <ashzadkamal@gmail.com>
2024-09-15 17:06:43 +0530

    Added Functions

--- app.py
+++ app.py
@@ -15,5 +15,24 @@ load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
+def get_odf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader=PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter= RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    
+    return chunks
+
+def get_vector_stoer(text_chunks):
+    embeddings= GoogleGenerativeAIEmbeddings(model="models/embeddings-001")
+    vectore_store=FAISS.from_texts(text_chunks, embedding=embeddings)
+    vectore_store.save_local("faiss_index")
+    
 
 
